b89be1b1dbab4695d8e99512dda51967c8caa388
cjmilner73 <chris@blueemoon.sg>
2023-01-22 19:30:00 +0800

    Automatic Commit

--- cron_dbapp.sh
+++ cron_dbapp.sh
@@ -1,3 +1,3 @@
 . /etc/bashrc
 . /Users/chrismilner/Bluemoon_Consulting/Projects/Flutter/app_back/venv/bin/activate
-python dbapp.py
+python /Users/chrismilner/Bluemoon_Consulting/Projects/Flutter/app_back/dbapp.py
