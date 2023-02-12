7e565e1fe950e49e857acf9e0e9adb4d8210633d
cjmilner73 <chris@blueemoon.sg>
2023-02-12 19:35:01 +0800

    Automatic Commit

--- dbapp.py
+++ dbapp.py
@@ -44,7 +44,7 @@ def get_holdings():
                 bodyDict = {"name": k, "id": myHolding['id'], "amount": myHolding['amount'], "last_price": v['usd'], "day_change": formatShortChange}
                 update_prices(k, bodyDict)
                 total = total + (myHolding['amount'] * v['usd'])
-                print(k, myHolding['amount'])               
+                print(k, myHolding['amount'] * v['usd'])               
                 # url = 'http://127.0.0.1:5000/holdings'
                 # x = requests.post(url, json = json.dumps(myobj))
                 # print(x.text)               
